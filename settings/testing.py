de93148df0f405c5204d887898ae24062b62dda9
Chris Vrolijk <Chris.Vrolijk@klimaathelpdesk.org>
2025-08-11 14:12:12 +0200

    remove teststuff

--- settings/testing.py
+++ settings/testing.py
@@ -1,33 +1,8 @@
-import dj_database_url 
 from .base import *  # NOQA
 
 
 SECRET_KEY = "CHANGEME!!!"
 
-DATABASES = {
-    
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "CONN_MAX_AGE": 600,
-        # number of seconds database connections should persist for
-        "NAME": "KlimaatHelpdeskDev",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PASSWORD": "Vfhsdbgtcfbt85456$%",
-    },
-    'postgres': {
-        "ENGINE": "django.db.backends.postgresql",
-        "CONN_MAX_AGE": 600,
-        # number of seconds database connections should persist for
-        "NAME": "KlimaatHelpdeskDev",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PASSWORD": "Vfhsdbgtcfbt85456$%",
-    },
-    'defaultRender': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:Vfhsdbgtcfbt85456$%@localhost/KlimaatHelpdeskDev',
-        #default='postgresql://klimaathelpdeskdev_user:jRrv7yfoUkVI5Xt437CU43PS9SfXEHlo@dpg-cvr7nsvgi27c738n8log-a/klimaathelpdeskdev',
-        conn_max_age=600
-    )
-}
+DATABASES["default"]["USER"] = "klimaathelpdesk"
+DATABASES["default"]["PASSWORD"] = "klimaathelpdesk"
+DATABASES["default"]["HOST"] = "localhost"
