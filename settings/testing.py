ed0d8539bdc4d0e87b5cca06e94ee29081773b5f
Chris Vrolijk <Chris.Vrolijk@klimaathelpdesk.org>
2025-08-11 13:20:56 +0200

    daily commit

--- settings/testing.py
+++ settings/testing.py
@@ -5,7 +5,17 @@ from .base import *  # NOQA
 SECRET_KEY = "CHANGEME!!!"
 
 DATABASES = {
+    
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "CONN_MAX_AGE": 600,
+        # number of seconds database connections should persist for
+        "NAME": "KlimaatHelpdeskDev",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PASSWORD": "Vfhsdbgtcfbt85456$%",
+    },
+    'postgres': {
         "ENGINE": "django.db.backends.postgresql",
         "CONN_MAX_AGE": 600,
         # number of seconds database connections should persist for
@@ -16,7 +26,7 @@ DATABASES = {
     },
     'defaultRender': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://klimaathelpdeskdev_user:jRrv7yfoUkVI5Xt437CU43PS9SfXEHlo@dpg-cvr7nsvgi27c738n8log-a.frankfurt-postgres.render.com/klimaathelpdeskdev',
+        default='postgresql://postgres:Vfhsdbgtcfbt85456$%@localhost/KlimaatHelpdeskDev',
         #default='postgresql://klimaathelpdeskdev_user:jRrv7yfoUkVI5Xt437CU43PS9SfXEHlo@dpg-cvr7nsvgi27c738n8log-a/klimaathelpdeskdev',
         conn_max_age=600
     )
