6ad4645b29204e54aa9728b512581e09d35571c5
Jelle de Bruijn <jellejacobdebruijn@gmail.com>
2025-09-17 12:17:12 +0200

    added localhost & excl. init from pytest-cov

--- settings/development.py
+++ settings/development.py
@@ -1,5 +1,5 @@
 import os
-import dj_database_url 
+import dj_database_url
 from .base import *  # NOQA
 
 
@@ -21,7 +21,7 @@ DATABASES = {
     )
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
