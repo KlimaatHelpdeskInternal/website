7f2b8fe8247d297c07dcb042b18a8280d5358c41
ChrisVrolijkKH <chris@vrolijk.com>
2026-02-03 16:08:18 +0100

    use render-test settings

--- render-hosting/PreDeployRenderTest.sh
+++ render-hosting/PreDeployRenderTest.sh
@@ -3,5 +3,5 @@
 set -o errexit
 python manage.py collectstatic --no-input --settings=settings.render-test
 # Apply any outstanding database migrations
-python manage.py makemigrations
+python manage.py makemigrations --settings=settings.render-test
 python manage.py migrate --settings=settings.render-test
