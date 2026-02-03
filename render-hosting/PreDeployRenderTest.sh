91c36501b168a781b18d14f1aac2ba1974d36c72
ChrisVrolijkKH <chris@vrolijk.com>
2026-02-03 16:06:05 +0100

    use render settigns in collectstatic

--- render-hosting/PreDeployRenderTest.sh
+++ render-hosting/PreDeployRenderTest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --settings=settings.render-test
 # Apply any outstanding database migrations
 python manage.py makemigrations
 python manage.py migrate --settings=settings.render-test
