62b46f29665889adb621462fa19c68970da93810
ChrisVrolijkKH <chris@vrolijk.com>
2026-02-12 12:46:40 +0100

    try different way of static files

--- settings/render-prod.py
+++ settings/render-prod.py
@@ -55,12 +55,13 @@ STATIC_URL = "/static/"
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    STORAGES["staticfiles"] = {  # noqa: F405
+    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+}
 
 GITLAB_PERSONAL_TOKEN= get_secret(
     os.getenv("GITLAB_PERSONAL_TOKEN_FILE", "/run/secrets/gitlab_personal_token"), ""
