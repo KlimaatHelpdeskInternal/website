f32a19b31eabaf635b534267c87c60508d614d68
Jos Velema <volumensolutions@gmail.com>
2026-02-13 12:03:28 +0100

    staticfiles to whitenoise

--- settings/render-prod.py
+++ settings/render-prod.py
@@ -78,21 +78,26 @@ LOGGING = {
     },
 }
 
-
-STATIC_ROOT = BASE_DIR / "static"
+# Render: schrijf static naar deze map (bestaat in runtime)
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STORAGES["staticfiles"] = {  # noqa: F405
+# Render: override ALLEEN staticfiles storage (laat default media S3 met rust)
+STORAGES["staticfiles"] = {
     "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
 }
+
+
+# # This production code might break development mode, so we check whether we're in DEBUG mode
+# if not DEBUG:
+#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+#     # and renames the files with unique names for each version to support long-term caching
+#     STORAGES["staticfiles"] = {  # noqa: F405
+#     "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# }
 
 GITLAB_PERSONAL_TOKEN= get_secret(
     os.getenv("GITLAB_PERSONAL_TOKEN_FILE", "/run/secrets/gitlab_personal_token"), ""
