fe6f3ab1d5abdf21f94f1ae4c372b0fa14474169
Jelle de Bruijn <jellejacobdebruijn@gmail.com>
2026-02-23 23:35:10 +0100

    merge origin website-testing

--- settings/development.py
+++ settings/development.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import os
-import dj_database_url
-=======
->>>>>>> origin/main
 from .base import *  # NOQA
 from dotenv import load_dotenv
-load_dotenv() 
+load_dotenv()
 INSTALLED_APPS += [
     # "debug_toolbar",
 ]
@@ -50,19 +45,3 @@ MEDIA_URL = "/media/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-<<<<<<< HEAD
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-try:
-    from .local import *  # NOQA
-except ImportError:
-    pass
-=======
->>>>>>> origin/main
