b4e1bdf9ba235c83fd30573be9f01f133e87e281
ChrisVrolijkKH <chris@vrolijk.com>
2026-03-19 10:51:21 +0100

    change predeploy render

--- render-hosting/PreDeployRenderTest.sh
+++ render-hosting/PreDeployRenderTest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-npm run --silent build 
+#npm run --quiet build 
 python manage.py collectstatic --no-input --settings=settings.render-test
 # Apply any outstanding database migrations
 python manage.py makemigrations --settings=settings.render-test
